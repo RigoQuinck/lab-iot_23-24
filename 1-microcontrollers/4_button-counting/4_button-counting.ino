2236389449d2b1d9a67e57c0aedd72a836765074
Stefano Righini <stefano.righini@quinck.io>
2024-02-18 11:59:32 +0100

    feat: ✨ add button-counting

---
+++ 1-microcontrollers/4_button-counting/4_button-counting.ino
@@ -0,0 +1,21 @@
+#define BUTTON_PIN 2
+
+int count;
+
+void setup()
+{
+  pinMode(BUTTON_PIN, INPUT);
+  Serial.begin(9600);
+  count = 0;
+}
+
+void loop()
+{
+  int buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState == HIGH)
+  {
+    count++;
+    Serial.print("Pressed - count: ");
+    Serial.println(count);
+  }
+}
