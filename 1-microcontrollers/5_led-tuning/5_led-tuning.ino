7bb03baef27dd7b8dae2d8439dcffb8e7f2fd161
Stefano Righini <stefano.righini@quinck.io>
2024-02-18 12:25:55 +0100

    feat: ✨ add led-tuning

---
+++ 1-microcontrollers/5_led-tuning/5_led-tuning.ino
@@ -0,0 +1,22 @@
+#define POT_PIN A0
+#define LED_PIN 10
+
+int current;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+  int newValue = analogRead(POT_PIN);
+  if (newValue != current)
+  {
+    current = newValue;
+    int intensity = current / 4;
+    analogWrite(LED_PIN, intensity);
+    Serial.println(current);
+  }
+}
