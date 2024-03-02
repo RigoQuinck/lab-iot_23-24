21dcdd466f40e6250f7797f3ecf0b5f6f1ee9c75
Stefano Righini <stefano.righini@quinck.io>
2024-03-02 16:29:52 +0100

    feat: ✨ add fsm

---
+++ 4-fsm/blinking_synch/blinking_synch.ino
@@ -0,0 +1,44 @@
+#include "src/Led.h"
+#include "src/Timer.h"
+
+#define LED_PIN 8
+
+Light *light;
+Timer timer;
+
+enum
+{
+  ON,
+  OFF
+} state;
+
+void setup()
+{
+  Serial.begin(9600);
+  light = new Led(LED_PIN);
+  state = OFF;
+  timer.setupPeriod(500);
+}
+
+void step()
+{
+  switch (state)
+  {
+  case OFF:
+    light->switchOn();
+    Serial.println("ON");
+    state = ON;
+    break;
+  case ON:
+    light->switchOff();
+    Serial.println("OFF");
+    state = OFF;
+    break;
+  }
+}
+
+void loop()
+{
+  timer.waitForNextTick();
+  step();
+};
---
+++ 4-fsm/remote_blinking/remote_blinking.ino
@@ -0,0 +1,33 @@
+#include "src/Led.h"
+#include "src/MsgService.h"
+
+#define LED_PIN 13
+
+Light *led;
+char data;
+
+void setup()
+{
+  led = new Led(LED_PIN);
+  MsgService.init();
+}
+
+void loop()
+{
+  if (MsgService.isMsgAvailable())
+  {
+    Msg *msg = MsgService.receiveMsg();
+    if (msg->getContent() == "1")
+    {
+      led->switchOn();
+      MsgService.sendMsg("LED ON");
+    }
+    else if (msg->getContent() == "0")
+    {
+      led->switchOff();
+      MsgService.sendMsg("LED OFF");
+    }
+    /* NOT TO FORGET: msg deallocation */
+    delete msg;
+  }
+};
