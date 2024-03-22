ba41e363891367b01eecc72e7505c5b668f8e43c
Stefano Righini <stefano.righini@quinck.io>
2024-03-22 23:41:43 +0100

    feat: ✨ add 6-events

---
+++ 6-events/pattern-observer-example/pattern-observer-example.ino
@@ -0,0 +1,45 @@
+/*
+ * Pattern observer example.
+ */
+#include "src/ButtonImpl.h"
+
+class MyListener : public ButtonListener {
+   public:
+    MyListener() {
+        count = 0;
+    }
+
+    void notifyButtonPressed() {
+        count++;
+    }
+
+    void notifyButtonReleased() {
+        count++;
+    }
+
+    int getCount() {
+        noInterrupts();
+        int c = count;
+        interrupts();
+        return c;
+    }
+
+   private:
+    volatile int count;
+};
+
+AbstractButton *buttonA, *buttonB;
+MyListener* listener;
+
+void setup() {
+    Serial.begin(9600);
+    buttonA = new ButtonImpl(4);
+    buttonB = new ButtonImpl(5);
+    listener = new MyListener();
+    buttonA->addListener(listener);
+    buttonB->addListener(listener);
+}
+
+void loop() {
+    Serial.println(listener->getCount());
+};
