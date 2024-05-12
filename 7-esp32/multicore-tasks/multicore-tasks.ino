d02d2fb97c86e574a23629af86dec57b8ed4cdd6
Stefano Righini <stefano.righini@quinck.io>
2024-05-12 11:52:20 +0200

    feat: ✨ update esp32 multicore-tasks

--- 7-esp32/multicore-tasks/multicore-tasks.ino
+++ 7-esp32/multicore-tasks/multicore-tasks.ino
@@ -7,13 +7,13 @@
 TaskHandle_t Task1;
 TaskHandle_t Task2;
 
-const int led_1 = 4;
-const int led_2 = 5;
+const int blueLed = 4;
+const int redLed = 16;
 
 void setup() {
     Serial.begin(115200);
-    pinMode(led_1, OUTPUT);
-    pinMode(led_2, OUTPUT);
+    pinMode(blueLed, OUTPUT);
+    pinMode(redLed, OUTPUT);
 
     xTaskCreatePinnedToCore(Task1code, "Task1", 10000, NULL, 1, &Task1, 0);
     delay(500);
@@ -27,9 +27,9 @@ void Task1code(void* parameter) {
     Serial.println(xPortGetCoreID());
 
     for (;;) {
-        digitalWrite(led_1, HIGH);
+        digitalWrite(blueLed, HIGH);
         delay(500);
-        digitalWrite(led_1, LOW);
+        digitalWrite(blueLed, LOW);
         delay(500);
     }
 }
@@ -39,9 +39,9 @@ void Task2code(void* parameter) {
     Serial.println(xPortGetCoreID());
 
     for (;;) {
-        digitalWrite(led_2, HIGH);
+        digitalWrite(redLed, HIGH);
         delay(1000);
-        digitalWrite(led_2, LOW);
+        digitalWrite(redLed, LOW);
         delay(1000);
     }
 }
