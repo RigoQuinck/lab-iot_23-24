44254861ffe824a71bd778aa90d21ceace4d3ca7
Stefano Righini <stefano.righini@quinck.io>
2024-05-18 17:45:41 +0200

    feat: ✨ update raspi bt ping pong

--- 8-raspberry-pi/bt-ping-pong/raspberry/raspberry.py
+++ 8-raspberry-pi/bt-ping-pong/raspberry/raspberry.py
@@ -1,42 +1,54 @@
 import bluetooth
 
 def find_device(device_name):
-    print(f"Scanning for Bluetooth devices with name {device_name}...")
+    print("Scanning for Bluetooth devices...")
     devices = bluetooth.discover_devices(duration=8, lookup_names=True, flush_cache=True, lookup_class=False)
 
-    for addr, name in devices:
-        if name == device_name:
-            print(f"Found device {name} with address {addr}")
-            return addr
-
-    print(f"Device {device_name} not found.")
+    if devices:
+        print(f"Found {len(devices)} devices:")
+        for addr, name in devices:
+            print(f"  {name} - {addr}")
+            if name == device_name:
+                print(f"Device {device_name} found with address {addr}")
+                return addr
+    else:
+        print("No devices found.")
     return None
 
 def connect_bluetooth(address):
     port = 1
     sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    sock.connect((address, port))
-    return sock
+    try:
+        print(f"Connecting to {address}...")
+        sock.connect((address, port))
+        print("Connected successfully!")
+        return sock
+    except bluetooth.btcommon.BluetoothError as err:
+        print(f"Failed to connect: {err}")
+        return None
 
 def main():
-    address = find_device(device_name='RIGOBT')
+    device_name = 'RIGOBT'  # Replace with your HC-05 device name
+    address = find_device(device_name)
     if address:
         sock = connect_bluetooth(address)
-
-        try:
-            while True:
-                data = input("Enter data to send: ")
-                if data.lower() == "exit":
-                    break
-                sock.send(data)
-                received = sock.recv(1024)
-                print(f"Received: {received.decode('utf-8')}")
-        except KeyboardInterrupt:
-            print("Disconnected")
-        finally:
-            sock.close()
+        if sock:
+            try:
+                while True:
+                    data = input("Enter data to send: ")
+                    if data.lower() == "exit":
+                        break
+                    sock.send(data)
+                    received = sock.recv(1024)
+                    print(f"Received: {received.decode('utf-8')}")
+            except KeyboardInterrupt:
+                print("Disconnected")
+            finally:
+                sock.close()
+        else:
+            print("Could not connect to the device.")
     else:
-        print("Device not found")
+        print(f"Device {device_name} not found.")
 
 if __name__ == "__main__":
     main()
