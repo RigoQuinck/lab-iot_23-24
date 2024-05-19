680b8be7d71358065743b1f61f33f7b1dad35084
Stefano Righini <stefano.righini@quinck.io>
2024-05-19 15:41:32 +0200

    feat: ✨ update raspi event-loop

--- 8-raspberry-pi/event-loop/event-loop.py
+++ 8-raspberry-pi/event-loop/event-loop.py
@@ -2,17 +2,21 @@ import asyncio
 import bluetooth
 from enum import Enum
 from typing import Dict, Tuple, Any
+import json
 
 # Events
-EventType = Enum('EventType', ['device_found','device_connected', 'device_connection_failed'])
+EventType = Enum('EventType', ['device_found','device_connected', 'device_connection_failed', 'data_received'])
 Event = Tuple[EventType, Dict[str,Any]]
 def event(event_type: EventType, data: Dict[str,str]) -> Event:
     return (event_type, data)
+
+Status = Enum('Status', ['connected','not_connected'])
 
 # Define event loop and event handlers
 class EventLoop:
     def __init__(self):
         self.event_queue = asyncio.Queue()
+        self.status = Status.not_connected
 
     async def run(self):
         while True:
@@ -23,6 +27,16 @@ class EventLoop:
             await asyncio.sleep(10)
 
     async def handle_event(self, event):
+        match self.status:
+            case Status.not_connected:
+                await self.not_connected_handler(event)
+
+    async def connected_handler(self, event):
+        match event:
+            case (EventType.data_received, json_data):
+                print(f"Received: {json_data}")
+
+    async def not_connected_handler(self, event):
         match event:
             case (EventType.device_found, data):
                 await self.handle_device_found_event(data)
@@ -35,10 +49,11 @@ class EventLoop:
         match data:
             case {"address": address, "name": name}:
                 print(f"Device {name} found with address {address}")
-                connect_bluetooth(self, address)
+                await connect_bluetooth(self, address)
     
     async def handle_device_connected_event(self, sock):
-        print(f"Device connected")
+        self.status = Status.connected
+        receive_data(self, sock)
     
     async def handle_device_connection_failed_event(self, data):
         match data:
@@ -46,14 +61,37 @@ class EventLoop:
                 print(f"Device with address {address} failed to connect")
                 print(err)
 
+
+async def receive_data(event_loop: EventLoop, sock):
+    try:
+        buffer = ""
+        while True:
+            data = sock.recv(1024).decode('utf-8')
+            buffer += data
+
+            # Cerca il carattere di nuova linea
+            while '\n' in buffer:
+                line, buffer = buffer.split('\n', 1)
+                if line:
+                    try:
+                        json_data = json.loads(line.strip())
+                        await event_loop.event_queue.put(event(EventType.data_received,json_data))
+                        # Puoi aggiungere ulteriori elaborazioni qui
+                    except json.JSONDecodeError:
+                        print(f"Errore nel decodificare JSON: {line}")
+    except:
+        sock.close()
+    finally:
+        sock.close()
+
 async def connect_bluetooth(event_loop: EventLoop, address: str):
     port = 1
     sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     try:
         sock.connect((address, port))
-        await event_loop.event_queue.put(Event(EventType.device_connected,sock))
+        await event_loop.event_queue.put(event(EventType.device_connected,sock))
     except bluetooth.btcommon.BluetoothError as err:
-        await event_loop.event_queue.put(Event(EventType.device_connection_failed,(err,address)))
+        await event_loop.event_queue.put(event(EventType.device_connection_failed,(err,address)))
     return None
 
 # Bluetooth scanner
